963f39b10ed944b7e818f801ceb1000c09ff8fde
thually <alejosalazarmejia.2000@gmail.com>
2020-09-22 22:05:19 -0500

    A pretty good template for the actual homework

--- src/main/java/HelloMaven.java
+++ src/main/java/HelloMaven.java
@@ -1,5 +1,17 @@
+import org.json.simple.JSONArray;
+
+import java.util.Scanner;
+
 public class HelloMaven {
     public static void main(String[] args) {
         System.out.println("Hello word");
+        int a, b;
+        a = 2;
+        b = 3;
+        System.out.println(a+b);
+        System.out.println("Hice un archivo ejecutable");
+        Scanner input = new Scanner(System.in);
+        String linea = input.nextLine();
+        System.out.println(linea);
     }
 }
---
+++ src/test/java/TestingTest.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import org.json.simple.JSONArray;
+
+public class TestingTest {
+    public static void main(String[] args) {
+        //I guess this folder is for testing files ans stuff.
+    }
+}
